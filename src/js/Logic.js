d90e6229341d309bbfa8a1557034801e8b53d6a2
Xokin <xokinxelden@gmail.com>
2025-05-07 23:34:43 +0300

    Доделал всё, осталось лишь навести порядок в паре файлов

--- src/js/Logic.js
+++ src/js/Logic.js
@@ -24,3 +24,77 @@ ShowHideBlock2.addEventListener('click', () => {
     console.log(ShowHide.style.left)
   }
 })
+
+let ShowHideBlockFeed1 = document.querySelector(
+  '.R-HideBlock-feedback__close-btn'
+)
+let ShowHideBlockFeed2 = document.querySelector('.icons__chat')
+let ShowHideFeed = document.querySelector('.R-HideBlock-feedback')
+
+ShowHideBlockFeed1.addEventListener('click', () => {
+  console.log('click')
+  if (ShowHideFeed.style.opacity === '0') {
+    console.log('Проявляю')
+    ShowHideFeed.style.opacity = '1'
+    ShowHideFeed.style.animation = 'RBShow 0.3s forwards'
+    ShowHideFeed.style.pointerEvents = 'auto'
+    console.log(ShowHideFeed.style.opacity)
+  } else if (ShowHideFeed.style.opacity === '1') {
+    console.log('Делаю прозрачным')
+    ShowHideFeed.style.opacity = '0'
+    ShowHideFeed.style.animation = 'RBHide 0.3s forwards'
+    ShowHideFeed.style.pointerEvents = 'none'
+  }
+})
+ShowHideBlockFeed2.addEventListener('click', () => {
+  console.log('click')
+  if (ShowHideFeed.style.opacity === '0') {
+    console.log('Проявляю')
+    ShowHideFeed.style.opacity = '1'
+    ShowHideFeed.style.animation = 'RBShow 0.3s forwards'
+    ShowHideFeed.style.pointerEvents = 'auto'
+    console.log(ShowHideFeed.style.opacity)
+  } else if (ShowHideFeed.style.opacity === '1') {
+    console.log('Делаю прозрачным')
+    ShowHideFeed.style.opacity = '0'
+    ShowHideFeed.style.animation = 'RBHide 0.3s forwards'
+    ShowHideFeed.style.pointerEvents = 'none'
+    console.log(ShowHideFeed.style.opacity)
+  }
+})
+
+let ShowHideBlockCall1 = document.querySelector('.R-HideBlock-call__close-btn')
+let ShowHideBlockCall2 = document.querySelector('.icons__call')
+let ShowHideCall = document.querySelector('.R-HideBlock-call')
+
+ShowHideBlockCall1.addEventListener('click', () => {
+  console.log('click')
+  if (ShowHideCall.style.opacity === '0') {
+    console.log('Проявляю')
+    ShowHideCall.style.opacity = '1'
+    ShowHideCall.style.animation = 'RBShow 0.5s forwards'
+    ShowHideCall.style.pointerEvents = 'auto'
+    console.log(ShowHideFeed.style.opacity)
+  } else if (ShowHideCall.style.opacity === '1') {
+    console.log('Делаю прозрачным')
+    ShowHideCall.style.opacity = '0'
+    ShowHideCall.style.animation = 'RBHide 0.5s forwards'
+    ShowHideCall.style.pointerEvents = 'none'
+  }
+})
+ShowHideBlockCall2.addEventListener('click', () => {
+  console.log('click')
+  if (ShowHideCall.style.opacity === '0') {
+    console.log('Проявляю')
+    ShowHideCall.style.opacity = '1'
+    ShowHideCall.style.animation = 'RBShow 0.5s forwards'
+    ShowHideCall.style.pointerEvents = 'auto'
+    console.log(ShowHideCall.style.opacity)
+  } else if (ShowHideCall.style.opacity === '1') {
+    console.log('Делаю прозрачным')
+    ShowHideCall.style.opacity = '0'
+    ShowHideCall.style.animation = 'RBHide 0.5s forwards'
+    ShowHideCall.style.pointerEvents = 'none'
+    console.log(ShowHideCall.style.opacity)
+  }
+})
